3c49ea2ef03770267c757f6a5fcb8c27b408cba2
Katherine <liponina.katya@yandex.ru>
2023-03-16 23:02:34 +0300

    fullfilled all requerments

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -2,6 +2,9 @@ package storage
 
 import (
 	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 // Result represents the Size function result
@@ -22,8 +25,9 @@ type DirSizer interface {
 type sizer struct {
 	// maxWorkersCount number of workers for asynchronous run
 	maxWorkersCount int
-
-	// TODO: add other fields as you wish
+	maxSet bool
+	cwcMutex sync.Mutex
+	currentWorkersCount int64
 }
 
 // NewSizer returns new DirSizer instance
@@ -31,7 +35,78 @@ func NewSizer() DirSizer {
 	return &sizer{}
 }
 
-func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	// TODO: implement this
-	return Result{}, nil
+var childrenAmount int64
+
+func (a *sizer) Size(ctx context.Context, d Dir) (res Result, err error) {
+	collector := make(chan Result)
+	errorChannel := make(chan error)
+	childrenAmount = 1
+	go a.exploreDir(ctx, d, collector, errorChannel)
+	for childrenAmount != 0 {
+		select {
+		case r, ok := <-collector:
+			if !ok {
+				goto done
+			}
+			res.Count += r.Count
+			res.Size += r.Size
+		case e := <-errorChannel:
+			return Result{}, errors.Join(errors.New("error occured in one of goroutines"), e)
+		}
+	}
+	done:
+	close(errorChannel)
+	return res, nil
+}
+
+func (a *sizer) exploreDir(ctx context.Context, d Dir, resultChannel chan Result, errorChannel chan error) () {
+	var res Result
+	directories, files, err := d.Ls(ctx)
+	defer atomic.AddInt64(&childrenAmount, -1)
+	defer func() {
+		if childrenAmount - 1 == 0 {
+			close(resultChannel)
+		}
+	}()
+	if err != nil {
+		errorChannel <- err
+		return
+	}
+	for _, f := range files {
+		res.Count++
+		size, err := f.Stat(ctx)
+		if err != nil {
+			errorChannel <- err
+			return
+		}
+		res.Size += size
+	}
+	
+	if !a.maxSet {
+		for _, dir := range directories {
+			atomic.AddInt64(&childrenAmount, 1)
+			go a.exploreDir(ctx, dir, resultChannel, errorChannel)
+		}
+		resultChannel <- res
+		return
+	}
+
+	for _, dir := range directories {
+		//for atomic operation
+		a.cwcMutex.Lock()
+		if a.currentWorkersCount < int64(a.maxWorkersCount) {
+			//possible to create goroutine
+			a.currentWorkersCount += 1
+			a.cwcMutex.Unlock()
+			atomic.AddInt64(&childrenAmount, 1)
+			go a.exploreDir(ctx, dir, resultChannel, errorChannel)
+		} else {
+			a.cwcMutex.Unlock()
+			//just continue on this goroutine
+			//use add 'cause function is ending althougt goroutine is not
+			atomic.AddInt64(&childrenAmount, 1)
+			a.exploreDir(ctx, dir, resultChannel, errorChannel)
+		}
+	}
+	resultChannel <- res
 }
