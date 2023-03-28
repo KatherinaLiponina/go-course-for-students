31ff46dd0bc469fe45b70ec9694db70c61a73160
Katherine <liponina.katya@yandex.ru>
2023-03-28 12:03:54 +0300

    discarded pipeline.go

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -12,24 +12,6 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	for i := 0; i < len(stages); i++ {
-		in = stages[i](in);
-	}
-	out := make(chan any)
-	go func() {
-		for {
-			select {
-			case r, ok := <-in:
-				if !ok {
-					close(out)
-					return
-				}
-				out <- r
-			case <-ctx.Done():
-				close(out)
-				return
-			}
-		}
-	}()
-	return out
+	// TODO
+	return nil
 }
