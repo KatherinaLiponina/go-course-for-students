3db3a7cbad96a7612e0fe24a500ddf42f50be3e0
Katherine <liponina.katya@yandex.ru>
2023-04-04 11:35:31 +0300

    requirement was split

--- lesson7/homework/validator.go
+++ lesson7/homework/validator.go
@@ -20,11 +20,11 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	var resultError string = ""
+	stringArray := make([]string, 0)
 	for i := 0; i < len(v); i++ {
-		resultError += ((v[i].Err.Error()) + ", ")
+		stringArray = append(stringArray, v[i].Err.Error())
 	}
-	return resultError[:len(resultError) - 2]
+	return strings.Join(stringArray, ", ")
 }
 
 func Validate(v any) error {
@@ -43,7 +43,14 @@ func Validate(v any) error {
 			continue
 		}
 		requirement := strings.Split(validation, ":")
-		if len(requirement) != 2 || requirement[1] == "" {
+		if len(requirement) < 2 {
+			err = append(err, ValidationError{ErrInvalidValidatorSyntax})
+			continue
+		}
+
+		validator := requirement[0]
+		value := requirement[1]
+		if value == "" {
 			err = append(err, ValidationError{ErrInvalidValidatorSyntax})
 			continue
 		}
@@ -56,19 +63,19 @@ func Validate(v any) error {
 		var e error = nil
 		switch typeLine {
 		case reflect.Int:
-			e = validateInteger(requirement, dv.Field(i).Interface().(int))
+			e = validateInteger(validator, value, dv.Field(i).Interface().(int))
 		case reflect.String:
-			e = validateString(requirement, dv.Field(i).Interface().(string))
+			e = validateString(validator, value, dv.Field(i).Interface().(string))
 		case reflect.Slice:
 			if dt.Field(i).Type.String() == "[]string" {
 				str := dv.Field(i).Interface().([]string)
 				for _, el := range(str) {
-					writeDownError(validateString(requirement, el), &err, dt.Field(i).Name + "." + strconv.Itoa(i))
+					writeDownError(validateString(validator, value, el), &err, dt.Field(i).Name + "." + strconv.Itoa(i))
 				}
 			} else if dt.Field(i).Type.String() == "[]int" {
 				str := dv.Field(i).Interface().([]int)
 				for _, el := range(str) {
-					writeDownError(validateInteger(requirement, el), &err, dt.Field(i).Name + "." + strconv.Itoa(i))
+					writeDownError(validateInteger(validator, value, el), &err, dt.Field(i).Name + "." + strconv.Itoa(i))
 				}
 			}
 			continue
@@ -111,14 +118,14 @@ func checkValue(value int, validator string, f func(int, int) bool) error {
 	return nil
 }
 
-func validateInteger(requirement []string, value int) error {
-	switch requirement[0] {
+func validateInteger(validation string, value string, val int) error {
+	switch validation {
 	case "min":
-		return checkValue(value, requirement[1], func(a, b int) bool { return a >= b })
+		return checkValue(val, value, func(a, b int) bool { return a >= b })
 	case "max":
-		return checkValue(value, requirement[1], func(a, b int) bool { return a <= b })
+		return checkValue(val, value, func(a, b int) bool { return a <= b })
 	case "in":
-		arr := strings.Split(requirement[1], ",")
+		arr := strings.Split(value, ",")
 		var intArr []int = make([]int, 0)
 		for _, el := range arr {
 			n, e := strconv.Atoi(el)
@@ -128,7 +135,7 @@ func validateInteger(requirement []string, value int) error {
 				intArr = append(intArr, n)
 			}
 		}
-		if !contains(intArr, value) {
+		if !contains(intArr, val) {
 			return ErrValidationError
 		}
 	default:
@@ -137,18 +144,18 @@ func validateInteger(requirement []string, value int) error {
 	return nil
 }
 
-func validateString(requirement []string, value string) error {
-	switch requirement[0] {
+func validateString(validation string, value string, val string) error {
+	switch validation {
 	case "len":
-		return checkValue(len(value), requirement[1], func(a, b int) bool { return a == b })
+		return checkValue(len(val), value, func(a, b int) bool { return a == b })
 	case "in":
-		if !contains(strings.Split(requirement[1], ","), value) {
+		if !contains(strings.Split(value, ","), val) {
 			return ErrValidationError
 		}
 	case "max":
-		return checkValue(len(value), requirement[1], func(a, b int) bool { return a < b })
+		return checkValue(len(val), value, func(a, b int) bool { return a < b })
 	case "min":
-		return checkValue(len(value), requirement[1], func(a, b int) bool { return a > b })
+		return checkValue(len(val), value, func(a, b int) bool { return a > b })
 	default:
 		return ErrInvalidValidatorSyntax
 	}
