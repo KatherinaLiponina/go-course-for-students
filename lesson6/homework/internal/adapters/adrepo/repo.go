3ed2ad0ce730de1fa0f942432053ec34703fd83e
Katherine <liponina.katya@yandex.ru>
2023-04-09 16:17:57 +0300

    homework 6

--- lesson6/homework/internal/adapters/adrepo/repo.go
+++ lesson6/homework/internal/adapters/adrepo/repo.go
@@ -1,9 +1,48 @@
 package adrepo
 
 import (
+	"errors"
+	"homework6/internal/ads"
 	"homework6/internal/app"
 )
 
+type repo struct {
+	adStorage map[int64]ads.Ad
+}
+
+func (r * repo) AppendAd(ad ads.Ad) {
+	r.adStorage[ad.ID] = ad
+}
+
+func (r * repo) ChangeAdStatus(ID int64, status bool) {
+	ad, _ := r.GetAdByID(ID)
+	ad.ChangeAdStatus(status)
+	r.adStorage[ad.ID] = *ad
+}
+
+func (r * repo) UpdateAd(ID int64, Text string, Title string) {
+	ad, _ := r.GetAdByID(ID)
+	if len(Text) > 0 {
+		ad.UpdateText(Text)
+	}
+	if len(Title) > 0 {
+		ad.UpdateTitle(Title)
+	}
+	r.adStorage[ad.ID] = *ad
+}
+
+func (r * repo) GetAdByID(ID int64) (*ads.Ad, error) {
+	a, ok := r.adStorage[ID]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+	return &a, nil
+}
+
+type repository struct {
+	*repo
+}
+
 func New() app.Repository {
-	return nil // TODO: реализовать
+	return repository{&repo{map[int64]ads.Ad{}}}
 }
--- lesson6/homework/internal/app/app.go
+++ lesson6/homework/internal/app/app.go
@@ -1,13 +1,62 @@
 package app
 
+import (
+	"errors"
+	"homework6/internal/ads"
+)
+
+var ErrNotFound = errors.New("Repository does not contain ad with given ID")
+var ErrForbidden = errors.New("AuthorID does not match given ID")
+
 type App interface {
-	// TODO: реализовать
+	CreateAd(Title string, Text string, AuthorID int64) (*ads.Ad, error)
+	ChangeAdStatus(ID int64, AuthorID int64, status bool) (*ads.Ad, error)
+	UpdateAd(ID int64, AuthorID int64, Title string, Text string) (*ads.Ad, error)
 }
 
 type Repository interface {
-	// TODO: реализовать
+	AppendAd(ad ads.Ad)
+	ChangeAdStatus(ID int64, status bool)
+	UpdateAd(ID int64, Text string, Title string)
+	GetAdByID(ID int64) (*ads.Ad, error)
+}
+
+type app struct {
+	index int64
+	repository Repository
+}
+
+func (a * app) CreateAd(Title string, Text string, AuthorID int64) (*ads.Ad, error) {
+	ad := ads.CreateAd(a.index, Title, Text, AuthorID)
+	a.index++
+	a.repository.AppendAd(ad)
+	return &ad, nil
+}
+
+func (a * app) ChangeAdStatus(ID int64, AuthorID int64, status bool) (*ads.Ad, error) {
+	ad, err := a.repository.GetAdByID(ID)
+	if err != nil {
+		return nil, ErrNotFound
+	}
+	if ad.AuthorID != AuthorID {
+		return nil, ErrForbidden
+	}
+	a.repository.ChangeAdStatus(ID, status)
+	return a.repository.GetAdByID(ID);
+}
+
+func (a * app) UpdateAd(ID int64, AuthorID int64, Title string, Text string) (*ads.Ad, error) {
+	ad, err := a.repository.GetAdByID(ID)
+	if err != nil {
+		return nil, ErrNotFound
+	}
+	if ad.AuthorID != AuthorID {
+		return nil, ErrForbidden
+	}
+	a.repository.UpdateAd(ID, Text, Title)
+	return a.repository.GetAdByID(ID);
 }
 
 func NewApp(repo Repository) App {
-	return nil // TODO: реализовать
+	return &app{repository: repo, index: 0}
 }
