5774203d2038767946f048133ab4340ebcb8fecf
Katherine <liponina.katya@yandex.ru>
2023-04-09 16:17:57 +0300

    homework 6

--- lesson6/homework/internal/ads/ads.go
+++ lesson6/homework/internal/ads/ads.go
@@ -7,3 +7,19 @@ type Ad struct {
 	AuthorID  int64
 	Published bool
 }
+
+func CreateAd(ID int64, Title string, Text string, AuthorID int64) Ad {
+	return Ad{ID, Title, Text, AuthorID, false}
+}
+
+func (a * Ad) ChangeAdStatus(status bool) {
+	a.Published = status
+}
+
+func (a * Ad) UpdateTitle(title string) {
+	a.Title = title
+}
+
+func (a * Ad) UpdateText(text string) {
+	a.Text = text
+}
