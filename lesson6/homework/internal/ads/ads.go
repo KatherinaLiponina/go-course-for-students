aba612d802626b7a002782a25c847866245d2e4c
Katherine <liponina.katya@yandex.ru>
2023-05-03 11:19:47 +0300

    fixed code for coverage flag

--- lesson6/homework/internal/ads/ads.go
+++ lesson6/homework/internal/ads/ads.go
@@ -12,14 +12,14 @@ func CreateAd(ID int64, Title string, Text string, AuthorID int64) Ad {
 	return Ad{ID, Title, Text, AuthorID, false}
 }
 
-func (a * Ad) ChangeAdStatus(status bool) {
+func (a *Ad) ChangeAdStatus(status bool) {
 	a.Published = status
 }
 
-func (a * Ad) UpdateTitle(title string) {
+func (a *Ad) UpdateTitle(title string) {
 	a.Title = title
 }
 
-func (a * Ad) UpdateText(text string) {
+func (a *Ad) UpdateText(text string) {
 	a.Text = text
 }
--- lesson9/homework/internal/users/users.go
+++ lesson9/homework/internal/users/users.go
@@ -10,10 +10,10 @@ func CreateUser(id int64, nick string, email string) User {
 	return User{id, nick, email}
 }
 
-func (u * User) UpdateNickname(n string) {
+func (u *User) UpdateNickname(n string) {
 	u.Nickname = n
 }
 
-func (u * User) UpdateEmail(e string) {
+func (u *User) UpdateEmail(e string) {
 	u.Email = e
 }
